19f267e0531a7f85443e75fa3ac6753260353838
Raven <ravenlb@yahoo.com>
2018-03-08 08:16:10 -0800

    lose the bgcolor
    and changed counter to title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 //initialize - set-up variables
 
-var counter = document.getElementById('counter');
+var title = document.getElementById('title');
 var lady = document.getElementById('lady');
 var ladyimg = document.getElementById('ladyimg');
 var storycontainer = document.getElementById('storycontainer');
@@ -27,10 +27,10 @@ console.log(windowHt);
 scrollFunction();
 
 //-ms-transform: translate(); /* IE 9 */
-counter.style.msTransform = "translateY(" + fromTop + "px)";
+title.style.msTransform = "translateY(" + fromTop + "px)";
 //-webkit-transform: translate(); /* Safari */
-counter.style.WebkitTransform = "translateY(" + fromTop + "px)";
-counter.style.transform = "translateY(" + fromTop + "px)";
+title.style.WebkitTransform = "translateY(" + fromTop + "px)";
+title.style.transform = "translateY(" + fromTop + "px)";
 
 //-ms-transform: translate(); /* IE 9 */
 lady.style.msTransform = "translateY(" + fromTop + "px)";
@@ -52,98 +52,98 @@ storycontainer.style.transform = "translateY(" + fromTop + "px)";
 
 
 if(fromTop > -10 && fromTop < 350){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+  //title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a fly.<br /> (scroll &#8595;)";
-  storycontainer.style.backgroundColor="#adebeb";
+//  storycontainer.style.backgroundColor="#adebeb";
 }else if(fromTop > 350 && fromTop < 715){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a fly.<br /> (scroll &#8595;)";
 }else if(fromTop > 715 && fromTop < 930){
-  counter.innerHTML = "I don't know why...";
-  counter.style.backgroundColor="#ff8080";
+  title.innerHTML = "I don't know why...";
+//  title.style.backgroundColor="#ff8080";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "she swallowed a fly.<br />Perhaps she'll die.";
   //flyfly.style.left="-10px";
 }else if(fromTop > 930 && fromTop < 1405){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a spider,<br />that wriggled, and jiggled,<br />and wiggled inside her.";
 }else if(fromTop > 1405 && fromTop < 1700){
-  counter.innerHTML = "She swallowed the spider...";
-  counter.style.backgroundColor="#ff6666";
+  title.innerHTML = "She swallowed the spider...";
+//  title.style.backgroundColor="#ff6666";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "to catch the fly.<br />I don't know why she<br />swallowed the fly!<br />Perhaps she'll die.";
 }else if(fromTop > 1700 && fromTop < 2225){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a bird.<br />How absurd,<br />to swallow a bird!";
 }else if(fromTop > 2225 && fromTop < 2695){
-  counter.innerHTML = "She swallowed the bird...";
-  counter.style.backgroundColor="#ff4d4d";
+  title.innerHTML = "She swallowed the bird...";
+//  title.style.backgroundColor="#ff4d4d";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "to catch the spider.<br />She swallowed the spider<br />to catch the fly.<br />I don't know why<br />she swallowed the fly,<br />perhaps she'll die.";
 }else if(fromTop > 2695 && fromTop < 3300){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a cat.<br />Imagine that,<br />she swallowed a cat!";
 }else if(fromTop > 3300 && fromTop < 4085){
-  counter.innerHTML = "She swallowed the cat...";
-  counter.style.backgroundColor="#ff3333";
+  title.innerHTML = "She swallowed the cat...";
+//  title.style.backgroundColor="#ff3333";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "to catch the bird.<br />She swallowed the bird<br />to catch the spider.<br />She swallowed the spider<br />to catch the fly.<br />I don't know why she swallowed the fly.<br />Perhaps she'll die.";
 }else if(fromTop > 4085 && fromTop < 4780){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a dog.<br />What a hog,<br />to swallow a dog!";
 }else if(fromTop > 4780 && fromTop < 5290){
-  counter.innerHTML = "She swallowed the dog...";
-  counter.style.backgroundColor="#ff1a1a";
+  title.innerHTML = "She swallowed the dog...";
+//  title.style.backgroundColor="#ff1a1a";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "to catch the cat.<br />She swallowed the cat<br />to catch the bird.<br />She swallowed the bird<br />to catch the spider.<br />She swallowed the spider<br />to catch the fly.<br />I don't know why she swallowed the fly.<br />Perhaps she'll die.";
 }else if(fromTop > 5290 && fromTop < 6075){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a goat.<br />Just opened her throat,<br />and swallowed a goat!";
 }else if(fromTop > 6075 && fromTop < 6780){
-  counter.innerHTML = "She swallowed the goat...";
-  counter.style.backgroundColor="#ff0000";
+  title.innerHTML = "She swallowed the goat...";
+//  title.style.backgroundColor="#ff0000";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "<h3>to catch the dog.<br />She swallowed the dog<br />to catch the cat.<br />She swallowed the cat<br />to catch the bird.<br />She swallowed the bird<br />to catch the spider.<br />She swallowed the spider<br />to catch the fly.<br />I don't know why she swallowed the fly.<br />Perhaps she'll die.</h3>";
 }else if(fromTop > 6780 && fromTop < 7650){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a cow.<br />I don't know how<br />she swallowed a cow!";
 }else if(fromTop > 7650 && fromTop < 8180){
-  counter.innerHTML = "She swallowed the cow...";
-  counter.style.backgroundColor="#cc0000";
+  title.innerHTML = "She swallowed the cow...";
+//  title.style.backgroundColor="#cc0000";
   ladyimg.src = 'images/old-lady-closed.png';
   story.innerHTML = "<h3>to catch the goat.<br />She swallowed the goat<br />to catch the dog.<br />She swallowed the dog<br />to catch the cat.<br />She swallowed the cat<br />to catch the bird.<br />She swallowed the bird<br />to catch the spider.<br />She swallowed the spider<br />to catch the fly.<br />I don't know why she swallowed the fly.<br />Perhaps she'll die.</h3>";
 }else if(fromTop > 8180 && fromTop < 9110){
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
   story.innerHTML = "who swallowed a horse.";
 }else if(fromTop > 9110 && fromTop < 10000){
-  counter.innerHTML = "She's dead, of course!";
-  counter.style.backgroundColor="#660000";
+  title.innerHTML = "She's dead, of course!";
+//  title.style.backgroundColor="#660000";
   ladyimg.src = 'images/old-lady-dead2.png';
   story.innerHTML = "<center>The End.</center>";
-  storycontainer.style.backgroundColor='#660000';
+//  storycontainer.style.backgroundColor='#660000';
 }else{
-  counter.innerHTML = "There was an Old Lady...";
-  counter.style.backgroundColor="#adebeb";
+  title.innerHTML = "There was an Old Lady...";
+//  title.style.backgroundColor="#adebeb";
   ladyimg.src = 'images/old-lady-2.png';
-  storycontainer.style.backgroundColor="#adebeb";
+//  storycontainer.style.backgroundColor="#adebeb";
 }
 }
 
